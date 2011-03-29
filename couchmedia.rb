f951850890bffe8aaad566d1424452b27b982067
leoc <leoc.git@gmail.com>
2011-03-29 12:05:18 +0200

    branched into rejected

--- couchmedia.rb
+++ couchmedia.rb
@@ -1,27 +1,16 @@
 require 'rubygems'
 require 'yaml'
-require 'mp3info'
 require 'eventmachine'
 require 'em-http'
 
-require 'lib/keyboard_handler.rb'
 require 'lib/changes_feed.rb'
 require 'lib/webservice.rb'
 
 @config = YAML::load_file('config.yml')
 
-def setup_changes_stream
-	
-end
-
 EventMachine.run do
-	# watch add folder
-	
-	# use the keyboard to emulate http requests
-	# debugging purpose
-	EM.open_keyboard KeyboardHandler
 	# couchmedia changes watch
-	ChangesFeed::stream @config
+	ChangesFeed::listen(@config)
 	# webserver (sinatra + thin)
 	Webservice.run! :port => 3000
 end
--- lib/changes_feed.rb
+++ lib/changes_feed.rb
@@ -1,32 +1,38 @@
 require 'json'
 
 class ChangesFeed
-	def self.stream config
-		@@last_seq ||= 0
-		req = EventMachine::HttpRequest.new("http://#{config['host']}:#{config['port']}/#{config['database']}/_changes?feed=continuous&since=#{@@last_seq}&heartbeat=30000").get(:timeout => 0)
-		req.stream do |chunk|
-			unless chunk.length < 2
- 				doc = JSON.parse(chunk)
- 				if doc['id'].nil?
-					@@last_seq = doc['last_seq']
- 				else
- 					@@last_seq = doc['seq']
-
-					puts doc.inspect
-					# handle all changes
- 					
-				end
- 			end
-  	end
-  	# handle possible errors of the stream
-		req.errback do
-			puts "error"
-		end
-		# reconnect the stream if we get disconnected
-		# this should not occur due to the heartbeat 
-		# couchdb is supposed to send each 30 seconds
-  	req.callback do
-  		stream config
-  	end
-	end
+  def self.register klass
+    @@handlers << klass
+  end
+  
+  def self.unregister klass
+    @@handlers.remove(klass)
+  end
+  
+  def self.stream config
+    @@last_seq ||= 0
+    req = EventMachine::HttpRequest.new("http://#{config['host']}:#{config['port']}/#{config['database']}/_changes?feed=continuous&since=#{@@last_seq}&heartbeat=30000").get(:timeout => 0)
+    req.stream do |chunk|
+      unless chunk.length < 2
+        doc = JSON.parse(chunk)
+        if doc['id'].nil?
+          @@last_seq = doc['last_seq']
+        else
+          @@last_seq = doc['seq']
+          puts doc.inspect
+          # handle all changes          
+        end
+      end
+    end
+    # handle possible errors of the stream
+    req.errback do
+      puts "error"
+    end
+    # reconnect the stream if we get disconnected
+    # this should not occur due to the heartbeat 
+    # couchdb is supposed to send each 30 seconds
+    req.callback do
+      stream config
+    end
+  end
 end
