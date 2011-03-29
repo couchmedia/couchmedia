c833435c5b9eab6955eb52afa870366d4599cbeb
leoc <leoc.git@gmail.com>
2011-03-29 12:08:41 +0200

    removed old file, for a fresh restart

--- lib/keyboard_handler.rb
+++
@@ -1,8 +0,0 @@
-module KeyboardHandler
-	include EM::Protocols::LineText2
-
-	def receive_line data
-		puts "received: #{data}"
-		# add files 
-	end
-end
--- plugins/sync/dummy_mp3_plugin/dummy.rb
+++
@@ -1,8 +0,0 @@
-class SyncPluginMP3
-  def self.syncDocToFile(document,file)
-    return false
-  end
-  def self.syncFileToDocs(file,documents)
-    return {}
-  end
-end
